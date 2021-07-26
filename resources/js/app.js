2c4450b938f4b91bf945440980d9fe4747b6aaac
Amar Diallo <amardiallo94@gmail.com>
2021-07-26 15:15:47 +0000

    Possiblite de partager un article sur les RS

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,10 @@ require('./bootstrap');
 
 window.Vue = require('vue').default;
 
+import VueSocialSharing from 'vue-social-sharing'
+
+Vue.use(VueSocialSharing);
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
--- resources/js/components/front/ArticleDetail.vue
+++ resources/js/components/front/ArticleDetail.vue
@@ -1,55 +1,94 @@
 <template>
-    <div>
-        <v-row>
-            <v-col cols="12" md="2"></v-col>
-            <v-col cols="12" md="8">
-                <h1 class="text-center">{{ article.name }}</h1>
-                <v-card class="mx-auto my-5" max-width="900" max-height="900" flat>
-                    <v-img
-                        class="white--text align-end"
-                        height="450px"
-                        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
-                    </v-img>
-                    <v-card-text class="text--primary">
-                        <div class="d-flex justify-space-between">
-                            <p class="text-left"><v-icon>person</v-icon> Autheur: {{ article.author }}</p>
-                            <p class="mr-16 text--grey"><v-icon>access_time</v-icon>
-                                {{ article.created_at }}
-                            </p>
-                        </div>
-                    </v-card-text>
-                </v-card>
+  <div>
+    <v-row>
+      <v-col cols="12" md="2"></v-col>
+      <v-col cols="12" md="8">
+        <h1 class="text-center">{{ article.name }}</h1>
+        <v-card class="mx-auto my-5" max-width="900" max-height="900" flat>
+          <v-img
+            class="white--text align-end"
+            height="450px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          >
+          </v-img>
+          <v-card-text class="text--primary">
+            <div class="d-flex justify-space-between">
+              <p class="text-left">
+                <v-icon>person</v-icon> Autheur: {{ article.author }}
+              </p>
+              <p class="mr-16 text--grey">
+                <v-icon>access_time</v-icon>
+                {{ article.created_at }}
+              </p>
+            </div>
+          </v-card-text>
+          <v-card-text>
+            <v-btn
+              v-for="icon in socialMedia"
+              :key="icon"
+              class="mx-4 black--text"
+              icon
+            >
+              <ShareNetwork
+                :network="icon.network"
+                url="https://news.vuejs.org/issues/180"
+                :title="article.name"
+                :description="article.description.substr(0, 100) + '...'"
+              >
+                <v-icon size="200%">
+                  {{ icon.icon }}
+                </v-icon>
+              </ShareNetwork>
+            </v-btn>
+          </v-card-text>
+        </v-card>
 
-                <p v-html="article.description"></p>
-            </v-col>
-            <v-col cols="12" md="2"></v-col>
-        </v-row>
-    </div>
+        <p v-html="article.description"></p>
+      </v-col>
+      <v-col cols="12" md="2"></v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            article: {},
-            errors: {},
+  data() {
+    return {
+      article: {},
+      errors: {},
+      socialMedia: [
+        {
+          icon: "mdi-facebook",
+          network: "facebook",
+        },
+        {
+          icon: "mdi-twitter",
+          network: "Twitter",
+        },
+        {
+          icon: "mdi-linkedin",
+          network: "linkedIn",
+        },
+      ],
+    };
+  },
+  methods: {
+    getArticle: async function () {
+      try {
+        let response = await axios.get(
+          "/article/" + this.$route.params.slug + "/show"
+        );
+        if (response.status == 200) {
+          this.article = response.data;
+          console.log(this.article);
         }
-    }, 
-    methods: {
-        getArticle: async function() {
-            try{
-                let response = await axios.get('/article/' + this.$route.params.slug + '/show');
-                if(response.status == 200) {
-                    this.article = response.data;
-                    console.log(this.article);
-                }
-            } catch(error) {
-                console.log(error);
-            }
-        }
+      } catch (error) {
+        console.log(error);
+      }
     },
-    created() {
-        this.getArticle();
-    }
+  },
+  created() {
+    this.getArticle();
+  },
 };
 </script>
