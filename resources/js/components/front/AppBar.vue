7b5d63973c9941acf0298fdc8cadc9c56faa137e
Amar Diallo <amardiallo94@gmail.com>
2021-08-03 14:13:02 +0000

    Ajout de la zone de recherche

--- resources/js/components/front/AppBar.vue
+++ resources/js/components/front/AppBar.vue
@@ -67,15 +67,73 @@
             {{ category.name }}
           </router-link>
         </template>
+        <v-icon color="white" size="40" @click="openSearch">mdi-card-search</v-icon>
       </div>
     </v-app-bar>
+
+    <v-parallax
+      class="mt-16"
+      dark
+      height="80"
+      v-if="isSearching"
+    >
+      <v-container fluid>
+        <v-layout justify-center row wrap>
+          <v-flex xs12 class="center mt-10">
+            <v-layout justify-center row wrap>
+              <v-flex xs6>
+                <v-form>
+                  <v-text-field
+                    outlined
+                    v-model="search"
+                    label="Rechercher..."
+                    append-icon="mdi-card-search"
+                    autofocus
+                  ></v-text-field>
+                </v-form>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-parallax>
+
+    <v-container fluid v-if="search != ''">
+      <v-layout justify-center row wrap>
+        <v-flex xs12 class="center mt-10">
+          <v-layout justify-center row wrap>
+            <v-flex xs6>
+              <v-list three-line >
+                <template v-for="(item, index) in filterArticle">
+                  <v-list-item :key="index" @click="openDetail(item)">
+                    <v-list-item-avatar>
+                      <v-img :src="'/uploads/' + item.image"></v-img>
+                    </v-list-item-avatar>
+
+                    <v-list-item-content>
+                      <v-list-item-title v-html="item.name"></v-list-item-title>
+                      <v-list-item-subtitle
+                        v-html="item.description.substr(0, 100) + '...'"
+                      ></v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+                  <v-divider :key="index"></v-divider>
+                </template>
+              </v-list>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
+      isSearching: false,
       selectedItem: 0,
+      search: "",
       items: [
         { icon: "mdi-view-dashboard", text: "Dashboard", route: "/dashboard" },
         { icon: "mdi-file", text: "Articles", route: "/articles" },
@@ -111,10 +169,18 @@ export default {
   mounted() {
     this.$store.dispatch("actifUser");
     this.$store.dispatch("getCategories");
+    this.$store.dispatch("getArticles");
   },
 
   methods: {
-    
+    openDetail(a) {
+      this.search = "";
+      this.isSearching = false;
+      this.$router.push('/' + a.category.slug + '/' + a.slug);
+    },
+    openSearch() {
+      return this.isSearching = !this.isSearching;
+    }
   },
 
   computed: {
@@ -126,7 +192,15 @@ export default {
     },
     categories() {
       return this.$store.state.categories;
-    }
+    },
+    articles() {
+      return this.$store.state.articles;
+    },
+    filterArticle() {
+      return this.articles.filter((article) => {
+        return article.name.match(this.search);
+      });
+    },
   },
 };
 </script>
